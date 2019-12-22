998df91abede88bd88f8068c39bc5f1f0ef83273
Pasha <pasha@unaik.com>
2019-12-22 11:50:21 +0200

    upd

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,36 +1,18 @@
-const path = require('path')
-const mix = require('laravel-mix')
+const path = require('path');
+const mix = require('laravel-mix');
 // const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
 
 mix.config.vue.esModule = true
 
 mix
-  .js('resources/js/app.js', 'public/js')
-  .sass('resources/sass/app.scss', 'public/css')
+  .js('resources/js/main.js', 'public/js')
+  .sass('resources/scss/global.scss', 'public/css')
 
   .sourceMaps()
   .disableNotifications()
 
 if (mix.inProduction()) {
   mix.version()
-
-  mix.extract([
-    'vue',
-    'vform',
-    'axios',
-    'vuex',
-    'jquery',
-    'popper.js',
-    'vue-i18n',
-    'vue-meta',
-    'js-cookie',
-    'bootstrap',
-    'vue-router',
-    'sweetalert2',
-    'vuex-router-sync',
-    '@fortawesome/vue-fontawesome',
-    '@fortawesome/fontawesome-svg-core'
-  ])
 }
 
 mix.webpackConfig({
